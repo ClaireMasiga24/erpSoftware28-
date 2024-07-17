bbc9e7e9b5c60d4914f15dd5b95a3406c6780e7f
Claire Masiga <ClaireMasiga22@gmail.com>
2024-07-17 10:08:29 +0300

    Made a login page

---
+++ login.js
@@ -0,0 +1,45 @@
+let loginButton = document.querySelector('button');
+let usernameInput = document.querySelector('#username');
+let passwordInput = document.querySelector('#password');
+
+function verifyLogin(username, password) {
+  const validUsernames = ['admin1', 'admin2', 'admin3'];
+  const validPassword = 'see';
+  return validUsernames.includes(username) && password === validPassword;
+}
+
+
+  if (verifyLogin(username, password)) {
+    let isAdmin = ['admin1', 'admin2', 'admin3'].includes(username);
+    window.location.href = isAdmin ? 'admin.html' : 'salesAgents.html';
+  } else {
+    alert('Invalid login credentials');
+  }
+
+loginButton.addEventListener('click', () => {
+  let username = usernameInput.value.trim();
+  let password = passwordInput.value.trim();
+
+  if (verifyLogin(username, password)) {
+    let isAdmin = ['admin1', 'admin2', 'admin3'].includes(username);
+    window.location.href = isAdmin ? 'admin.html' : 'salesAgents.html';
+  } else {
+    alert('Invalid login credentials');
+  }
+
+  // Reset the form
+  usernameInput.value = '';
+  passwordInput.value = '';
+});
+
+//Mr.Andrew's answer to redirection I will add event listeners to the link to sales agentpage
+//First I'm adding a script tag to the id
+let salesAgentsLink = document.querySelector('#sales-agents-link');
+
+  salesAgentsLink.addEventListener('click', () => {
+    window.location.href = 'salesAgents.html';
+  });
+  //Now the code for redirection
+  
+
+
